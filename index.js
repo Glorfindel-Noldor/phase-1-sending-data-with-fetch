6620573e764fb7968afc5e657af6e84aab9bd747
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2023-07-01 17:17:48 -0500

    this one was very frustrating

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+let user={
+    name:'Steve',
+    email:'steve@steve.com',
+}
+
+
+function submitData(x){
+    x = fetch('http://localhost:3000/users', configurationObject)
+    .then(res  =>res.json())
+    .then(data =>{
+        const newID = data.id;
+        const newElement = document.createElement('p');
+        newElement.textContent = 'new user id:\t' + newID;
+        document.body.appendChild(newElement);
+        console.log('data submited', data);
+    })
+    .catch(error => {
+        const noElement = document.createElement('p');
+        noElement.textContent = 'no user created:\t' + error;
+        document.body.appendChild(noElement);
+        console.error('data NOT submited', error)
+    })
+    return x;
+}
+
+
+
+const configurationObject = ({
+    method: "POST",
+    headers: {
+        "Content-Type" : "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(user),
+});
